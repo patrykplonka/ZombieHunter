b64a2addb9c12e6008344ec2c23de665884c3982
ZelseeH <122904100+ZelseeH@users.noreply.github.com>
2024-12-17 20:45:30 +0100

    hoho

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -4,7 +4,11 @@ public class Gun : MonoBehaviour
 {
     [Header("Prefabs")]
     [SerializeField] GameObject muzzle;
+<<<<<<< Updated upstream
     [SerializeField] Transform muzzlePostition;
+=======
+    [SerializeField] Transform muzzlePosition;
+>>>>>>> Stashed changes
     [SerializeField] GameObject projectile;
 
     [Header("Config")]
@@ -23,6 +27,7 @@ public class Gun : MonoBehaviour
         anim = GetComponent<Animator>();
         timeSinceLastShot = fireRate;
         player = GameObject.Find("Player").transform;
+<<<<<<< Updated upstream
         SetOffset(new Vector2(1, 0.5f));
     }
     private void Update()
@@ -92,3 +97,76 @@ offset = o;
 
 }
 }
+=======
+
+        SetOffset(new Vector2(1, 0.5f));
+    }
+
+    private void Update()
+    {
+        transform.position = (Vector2)player.position + offset;
+
+        FindClosestEnemy();
+        AimAtEnemy();
+        Shooting();
+
+    }
+
+    void FindClosestEnemy()
+    {
+        closestEnemy = null;
+        float closestDistance = Mathf.Infinity;
+
+
+        Enemy[] enemies = FindObjectsOfType<Enemy>();
+
+        foreach (Enemy enemy in enemies)
+        {
+            float distance = Vector2.Distance(transform.position, enemy.transform.position);
+            if ( distance < closestDistance && distance <= fireDistance)
+            { 
+                closestDistance = distance;
+                closestEnemy = enemy.transform;
+            }
+        }
+    }
+    void AimAtEnemy()
+    {
+        if (closestEnemy != null)
+        {
+            Vector3 direction = closestEnemy.position - transform.position;
+            direction.Normalize();
+            float angle = Mathf.Atan2(direction.y, direction.x) *Mathf.Rad2Deg;
+
+            transform.rotation = Quaternion.Euler(0,0,angle);
+            transform.position = (Vector2)player.position+offset;
+        } 
+    }
+
+    void Shooting()
+    {
+        if (closestEnemy == null) return;
+        timeSinceLastShot += Time.deltaTime;
+        if(timeSinceLastShot >= fireRate)
+        {
+            Shoot();
+            timeSinceLastShot = 0;
+        }
+    }
+
+    void Shoot()
+    {
+        var muzzleGo = Instantiate(muzzle, muzzlePosition.position, transform.rotation);
+        muzzleGo.transform.SetParent(transform);
+        Destroy(muzzleGo, 0.05f);
+
+        var projectileGo = Instantiate(projectile, muzzlePosition.position, transform.rotation);
+        Destroy(projectileGo, 3);
+    }
+    public void SetOffset(Vector2 o)
+    {
+        offset = o;
+    }
+
+}
+>>>>>>> Stashed changes
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 
+<<<<<<< Updated upstream
 public class Projectile : MonoBehaviour
 {
    float speed = 12f;
@@ -21,4 +22,24 @@ enemy.Hit(25);
     }
 
    }
+=======
+public class Projectille : MonoBehaviour
+{
+    float speed = 12f;
+
+    private void FixedUpdate()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var enemy = collision.gameObject.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            Destroy(gameObject);
+            enemy.Hit(25);
+        }
+    }
+>>>>>>> Stashed changes
 }
